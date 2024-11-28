3281f92eb589a334a129b821eb15117818dbcb74
YevheniiaZalevska <150593064+YevheniiaZalevska@users.noreply.github.com>
2024-11-28 12:03:46 +0000

    Deploying to gh-pages from @ YevheniiaZalevska/Recipes@90681cb7cc73d25c7a6941adb4a05e5372572c2a 🚀

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as d}from"./assets/vendor-DzcBeMgS.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))c(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function o(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(t){if(t.ep)return;t.ep=!0;const r=o(t);fetch(t.href,r)}})();async function u(){const{data:e}=await d("https://dummyjson.com/recipes");return e}async function m(e){const{data:i}=await d(`https://dummyjson.com/recipes/${e}`);return i}const s={recipeList:document.querySelector(".recipe-list"),backdrop:document.querySelector(".backdrop"),modal:document.querySelector(".modal")};function f(){s.backdrop.classList.remove("is-hidden")}function y(){s.backdrop.classList.add("is-hidden")}function g(e){const i=e.map(({image:o,id:c})=>`<li class="recipe-card">
+  <img src="${o}" alt="" data-id="${c}">
+</li>`).join("");s.recipeList.insertAdjacentHTML("beforeend",i)}function h({name:e,ingredients:i,instructions:o,cookTimeMinutes:c,servings:t,difficulty:r,image:n},l){const p=`<div class="modal">
+  <div class="modal-img"><img src="${n}" alt="${e}" /></div>
+  <div class="modal-description">
+    <h3 class="modal-title">Name: ${e}</h3>
+    <p class="modal-chapter">Ingredients:</p>
+    <ul class="modal-chapter-text">${i.map(a=>`<li>${a}</li>`).join("")}</ul>
+    <p class="modal-chapter">Instructions:</p>
+    <ul class="modal-chapter-text">${o.map(a=>`<li>${a}</li>`).join("")}</ul>
+    <span> Cook Time: ${c} </span>
+    <span>Servings: ${t}</span>
+    <span>Difficulty: ${r}</span>
+  </div>
+</div>`;l.innerHTML=p}async function L(e){if(e.target.nodeName!=="IMG")return;f();const i=e.target.dataset.id,o=await m(i);h(o,s.modal)}function v(e){e.target===e.currentTarget&&y()}document.addEventListener("DOMContentLoaded",async()=>{const{recipes:e}=await u();console.log(e),g(e)});s.recipeList.addEventListener("click",L);s.backdrop.addEventListener("click",v);
 //# sourceMappingURL=index.js.map
